8471fead25836099ae8efa53e8f5f5ac34885a11
Victor <victoride013@gmail.com>
2025-09-08 12:28:07 +0100

    google added

--- components/providers.tsx
+++ components/providers.tsx
@@ -8,16 +8,30 @@ import { CartProvider } from "@/contexts/cart-context"
 import { WishlistProvider } from "@/contexts/wishlist-context"
 import { OrderTrackingProvider } from "@/contexts/order-tracking-context"
 import UIResponseModalProvider from "@/components/providers/ui-response-modal-provider"
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
+  const content = (
+    <>
+      {children}
+      <UIResponseModalProvider />
+    </>
+  )
+
   return (
     <ReduxProvider>
       <ReduxAuthProvider>
         <CartProvider>
           <WishlistProvider>
             <OrderTrackingProvider>
-              {children}
-              <UIResponseModalProvider />
+              {googleClientId ? (
+                <GoogleOAuthProvider clientId={googleClientId}>
+                  {content}
+                </GoogleOAuthProvider>
+              ) : (
+                content
+              )}
             </OrderTrackingProvider>
           </WishlistProvider>
         </CartProvider>
